38b90998323168f6384ca96171eddc28dcad4415
juteexj <vo.yar58@gmail.com>
2019-07-18 19:50:32 +0300

    step three

--- src/Cli.php
+++ src/Cli.php
@@ -2,9 +2,6 @@
 
 namespace Braingames\Cli;
 
-use function \cli\line;
-use function \cli\prompt;
-
 $autoloadPath1 = __DIR__ . '/../../../autoload.php';
 $autoloadPath2 = __DIR__ . '/../vendor/autoload.php';
 if (file_exists($autoloadPath1)) {
@@ -13,7 +10,11 @@ if (file_exists($autoloadPath1)) {
     require_once $autoloadPath2;
 }
 
-function run () {
+use function \cli\line;
+use function \cli\prompt;
+
+function run()
+{
     line('Welcome to the Brain Game!');
     $name = \cli\prompt('May I have your name?');
     line("Hello, %s!", $name);
