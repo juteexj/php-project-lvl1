fb914e9d44806a67b4cc58f75d93c688ed03438a
juteexj <vo.yar58@gmail.com>
2019-07-22 15:33:47 +0300

    step four

--- src/Cli.php
+++ src/Cli.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Braingames\Cli;
+namespace BrainGames\Cli;
 
 use function \cli\line;
 use function \cli\prompt;
@@ -8,6 +8,8 @@ use function \cli\prompt;
 function run()
 {
     line('Welcome to the Brain Game!');
+    line("Answer \"yes\" if number even otherwise answer \"no\".\n
+    Any other answer will be considered an error.\n");
     $name = \cli\prompt('May I have your name?');
     line("Hello, %s!", $name);
 }
