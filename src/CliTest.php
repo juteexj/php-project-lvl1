f04cfdfef5708505de0a872b9353e2e710401786
juteexj <vo.yar58@gmail.com>
2019-07-26 12:26:18 +0300

    add test & fixes

--- src/CliTest.php
+++ src/CliTest.php
@@ -15,33 +15,6 @@ function calc()
     $operator = array("+", "-", "*");
     $randOperator = $operator[rand(0, 2)];
     $numbers = range(1, 100);
-    $rand1 = $numbers[rand(0, 99)];
-    $rand2 = $numbers[rand(0, 10)];
-    $rand3 = $numbers[rand(0, 99)];
-    $rand4 = $numbers[rand(0, 10)];
-    $rand5 = $numbers[rand(0, 99)];
-    $rand6 = $numbers[rand(0, 3)];
-    $rand7 = $numbers[rand(0, 99)];
-    $array = [$rand1, $rand2, $rand3, $rand4, $rand5, $rand6, $rand7];
 
-    foreach ($operator as $key => $value) {
-        if ($value == "+") {
-            $result = ("$rand1 + $rand2");
-            $res = $rand1 + $rand2;
-        } elseif ($value == "-") {
-            $result = ("$rand3 - $rand4");
-            $res = $rand3 - $rand4;
-        } elseif ($value == "*") {
-            $result = ("$rand5 * $rand6");
-            $res = $rand5 * $rand6;
-        }
-        line("Question: {$result}");
-        $answer = \cli\prompt("Your answer \n");
-        if ($answer == $res) {
-            line("Correct! \n");
-        } elseif ($answer != $res) {
-            line("{$answer} is wrong answer ;( Correct answer was {$res}. Let's try again, {$name}!");
-            break;
-        }
-    }
+
 }
